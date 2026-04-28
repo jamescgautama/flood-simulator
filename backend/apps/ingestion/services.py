4715703f411221503ef2f9c4d6a6da51d1ddf038
jamescgautama <jamescgautama@proton.me>
2026-04-28 21:00:52 +0700

    added env variables

--- backend/apps/ingestion/services.py
+++ backend/apps/ingestion/services.py
@@ -1,5 +1,9 @@
 import requests
 import xml.etree.ElementTree as ET
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
 def fetch_river_data():
     try:
@@ -21,7 +25,7 @@ def fetch_rainfall_data():
         url = (
             "https://api.openweathermap.org/data/2.5/weather"
             "?lat=-6.198&lon=106.810"
-            "&appid=57b9c70d05ecc12593cc199c9a2fa4ef"
+            f"&appid={API_KEY}"
         )
         response = requests.get(url, timeout=10)
         response.raise_for_status()
